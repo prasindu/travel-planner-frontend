f614056ac541f27b575e33928b078b6a2e81c7b2
prasindu <prasindudeshan1@gmail.com>
2026-05-01 11:38:37 +0530

    base url update

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://travel-api-srilanka-e6azatbsh0cna2bg.southeastasia-01.azurewebsites.net/api',
   timeout: 15000
 })
 
